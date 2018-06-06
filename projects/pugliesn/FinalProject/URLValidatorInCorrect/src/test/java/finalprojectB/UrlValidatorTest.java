0f0031084d61b17a72c8b4dc835adbbd61e45167
Nick Pugliese <nicholas.d.pugliese@gmail.com>
2018-06-05 18:40:07 -0700

    added manual and partition tests

--- projects/pugliesn/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/pugliesn/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -2,6 +2,11 @@
 package finalprojectB;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+import java.io.*;
 
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
@@ -14,37 +19,81 @@ import junit.framework.TestCase;
 public class UrlValidatorTest extends TestCase {
 
 
-   public UrlValidatorTest(String testName) {
-      super(testName);
+  //public UrlValidatorTest(String testName) {
+  //    super(testName);
+  //}
+/*
+   @Test(timeout = 4000)
+   public void testManualTest() {
+     UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+     //You can use this function to implement your manual testing
+
+     System.out.println("Manual tests: ");
+
+       if(urlVal.isValid("http://www.google.com")) {
+         System.out.println("Valid URL");
+       } else {
+         System.out.println("Invalid URL");
+       }
+        if(urlVal.isValid("http://www.google.com/whatever/test")) {
+         System.out.println("Valid URL");
+       } else {
+         System.out.println("Invalid URL");
+       }
+
+       try {
+         if(urlVal.isValid("h3t://www.google.com")) {
+           System.out.println("Valid URL");
+         } else {
+           System.out.println("Invalid URL");
+         }
+       } catch (Exception e) {
+         e.printStackTrace();
+       }
+
+       if(urlVal.isValid("www.google.com")) {
+         System.out.println("Valid URL");
+       } else {
+         System.out.println("Invalid URL");
+       }
+
+       if(urlVal.isValid("www.google.xd")) {
+         System.out.println("Valid URL");
+       } else {
+         System.out.println("Invalid URL");
+       }
+
+   }
+*/
+   @Test(timeout = 4000)
+   public void testPartition() {
+	 //You can use this function to implement your First Partition testing
+   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+   //You can use this function to implement your manual testing
+
+   System.out.println("Input partition tests: ");
+
+     if(urlVal.isValid("http://www.google.com")) {
+       System.out.println("Valid URL");
+     } else {
+       System.out.println("Invalid URL");
+     }
+
+     if(urlVal.isValid("file:///C:/Users/Nick/Downloads/FinalProject.pdf")) {
+       System.out.println("Valid URL");
+     } else {
+       System.out.println("Invalid URL");
+     }
+
+
    }
 
-   
-   
-   public void testManualTest()
-   {
-//You can use this function to implement your manual testing	   
-	   
-   }
-   
-   
-   public void testYourFirstPartition()
-   {
-	 //You can use this function to implement your First Partition testing	   
-
-   }
-   
-   public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
-
-   }
-   //You need to create more test cases for your Partitions if you need to 
-   
-   public void testIsValid()
-   {
+   @Test(timeout = 4000)
+   public void testWhiteBox() {
 	   //You can use this function for programming based testing
 
    }
-   
+
 
 
 }
